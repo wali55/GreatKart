da30d683e1b70ad2db4c7140c4652f762751519d
wali55 <walisantunu@gmail.com>
2024-02-18 23:23:37 +0600

    category finish

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -123,6 +123,10 @@ STATICFILES_DIRS = [
     'greatkart/static',
 ]
 
+# Media file configuration
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR /'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
--- greatkart/urls.py
+++ greatkart/urls.py
@@ -23,4 +23,4 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
