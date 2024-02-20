62db47242dc7ab45d40a7a2aa3fd6ff2304b182e
wali55 <walisantunu@gmail.com>
2024-02-20 21:29:57 +0600

    store page

--- store/models.py
+++ store/models.py
@@ -3,7 +3,7 @@ from category.models import Category
 
 class Product(models.Model):
     product_name = models.CharField(max_length=200, unique=True)
-    slug = models.SlugField(max_length=200, unique=200)
+    slug = models.SlugField(max_length=200, unique=True)
     description = models.TextField(max_length=500, blank=True)
     price = models.IntegerField()
     images = models.ImageField(upload_to='photos/products')
