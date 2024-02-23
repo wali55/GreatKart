b2d3389c99dcb901425220fdd344cca7166fbb02
wali55 <walisantunu@gmail.com>
2024-02-23 15:18:17 +0600

    cart

---
+++ carts/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Cart(models.Model):
+    cart_id = models.CharField(max_length=250, blank=True)
+    date_added = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.cart_id
