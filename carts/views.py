ac2e78ff05c183505cc268f001b3587fe1fa78f3
wali55 <walisantunu@gmail.com>
2024-02-27 14:39:31 +0600

    adding search functionality

--- carts/views.py
+++ carts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from store.models import Product
 from .models import Cart, CartItem
+from django.core.exceptions import ObjectDoesNotExist
 
 def _cart_id(request):
     cart = request.session.session_key
@@ -57,13 +58,15 @@ def cart(request, total=0, quantity=0, cart_items=None):
     cart_items = CartItem.objects.filter(cart=cart, is_active=True)
 
     try:
+        tax = 0
+        grand_total = 0
         for cart_item in cart_items:
             total += (cart_item.product.price * cart_item.quantity)
             quantity += cart_item.quantity
         
         tax = (2 * total) / 100
         grand_total = total + tax
-    except Cart.DoesNotExist:
+    except ObjectDoesNotExist:
         pass 
 
     context = {
