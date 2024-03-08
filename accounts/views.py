ff96b6a8b6786931e89515eee3f4d8ce58ba2552
wali55 <walisantunu@gmail.com>
2024-03-08 23:10:58 +0600

    register

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .forms import RegistrationForm
+from .models import Account
+from django.contrib import messages, auth
 
 def register(request):
-    return render(request, 'accounts/register.html')
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            email = form.cleaned_data['email']
+            username = email.split('@')[0]
+            phone_number = form.cleaned_data['phone_number']
+            password = form.cleaned_data['password']
+
+            user = Account.objects.create_user(
+                first_name = first_name,
+                last_name = last_name,
+                username = username,
+                email = email,
+                password = password
+            )
+
+            user.phone_number = phone_number
+            user.save()
+            messages.success(request, 'Registration successful')
+            return redirect('register')
+
+    else:
+        form = RegistrationForm()
+    
+        context = {
+            'form': form,
+        }
+        return render(request, 'accounts/register.html', context)
 
 def login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = auth.authenticate(email=email, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid login credentials.')
+            return redirect('login')
+
     return render(request, 'accounts/login.html')
 
 def logout(request):
--- greatkart/settings.py
+++ greatkart/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -138,3 +139,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # For custom user model
 AUTH_USER_MODEL = 'accounts.Account'
+
+MESSAGE_TAGS = {
+    messages.ERROR: "danger",
+}
