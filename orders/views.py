21b558209291046e85553b17f5553da5d4ce43b7
wali55 <walisantunu@gmail.com>
2024-03-26 18:35:20 +0600

    start after order tasks

--- orders/views.py
+++ orders/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render, redirect
 from carts.models import CartItem
 from .forms import OrderForm
-from .models import Order
+from .models import Order, Payment
 import datetime
+import json
 
 def payments(request):
+    body = json.loads(request.body)
+    order = Order.objects.get(user=request.user, is_ordered=False, order_number=body['orderID'])
+    
+    # Store transaction details in the Payment model.
+    payment = Payment(
+        user = request.user,
+        payment_id = body['transID'],
+        payment_type = body['payment_method'],
+        amount_paid = order.order_total,
+        status = body['status'],
+    )
+    payment.save()
+
+    order.payment = payment
+    order.is_ordered = True
+    order.save()
     return render(request, 'orders/payments.html')
 
 def place_order(request, total=0, quantity=0):
